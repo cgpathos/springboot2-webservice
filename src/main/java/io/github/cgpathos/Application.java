3293b45c8650500346d160d0a91bee0f8fcd9272
pathos <neopathos@me.com>
2020-05-28 01:22:06 +0900

    jpa crud 작성

--- src/main/java/io/github/cgpathos/Application.java
+++ src/main/java/io/github/cgpathos/Application.java
@@ -2,7 +2,9 @@ package io.github.cgpathos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 class Application {
     public static void main(String[] args) {
---
+++ src/main/java/io/github/cgpathos/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package io.github.cgpathos.web.dto;
+
+import io.github.cgpathos.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
