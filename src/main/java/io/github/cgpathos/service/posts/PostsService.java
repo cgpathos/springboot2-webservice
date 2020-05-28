fa9fb68dc1339d725d884133c69f8017971094b4
pathos <neopathos@me.com>
2020-05-29 01:24:25 +0900

    mustache를 이용한 간단한 게시판 구현

--- src/main/java/io/github/cgpathos/service/posts/PostsService.java
+++ src/main/java/io/github/cgpathos/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package io.github.cgpathos.service.posts;
 
 import io.github.cgpathos.domain.posts.Posts;
 import io.github.cgpathos.domain.posts.PostsRepository;
+import io.github.cgpathos.web.dto.PostsListResponseDto;
 import io.github.cgpathos.web.dto.PostsResponseDto;
 import io.github.cgpathos.web.dto.PostsSaveRequestDto;
 import io.github.cgpathos.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,10 +33,25 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(()
+            -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(()
             -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+            .map(PostsListResponseDto::new)
+            .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/io/github/cgpathos/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package io.github.cgpathos.web.dto;
+
+import io.github.cgpathos.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
