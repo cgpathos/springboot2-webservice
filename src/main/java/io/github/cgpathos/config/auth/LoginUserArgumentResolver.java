f6004a9a4e5ecae85a621a36a20693993df839e2
pathos <neopathos@me.com>
2020-06-02 01:23:53 +0900

    인증 정보 주입(아마도) 추가, 네이버 로그인 추가, 기존 테스트 코드에 인증관련 오류 수정

---
+++ src/main/java/io/github/cgpathos/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,30 @@
+package io.github.cgpathos.config.auth;
+
+import io.github.cgpathos.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = null != parameter.getParameterAnnotation(LoginUser.class);
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
--- src/main/java/io/github/cgpathos/web/IndexController.java
+++ src/main/java/io/github/cgpathos/web/IndexController.java
@@ -1,5 +1,6 @@
 package io.github.cgpathos.web;
 
+import io.github.cgpathos.config.auth.LoginUser;
 import io.github.cgpathos.config.auth.dto.SessionUser;
 import io.github.cgpathos.service.posts.PostsService;
 import io.github.cgpathos.web.dto.PostsResponseDto;
@@ -18,10 +19,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(null != user) {
+        if (null != user) {
             model.addAttribute("userName", user.getName());
         }
         return "index";
