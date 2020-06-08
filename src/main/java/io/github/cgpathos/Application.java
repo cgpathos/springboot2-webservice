b406c707fbd6b73ab16491f0e57ec6827b465081
pathos <neopathos@me.com>
2020-06-08 22:26:51 +0900

    application 클래스 public으로 변경, 코드 정리

--- src/main/java/io/github/cgpathos/Application.java
+++ src/main/java/io/github/cgpathos/Application.java
@@ -2,11 +2,10 @@ package io.github.cgpathos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing
 @SpringBootApplication
-class Application {
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
