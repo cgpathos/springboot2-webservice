8da00d1bb7818dd47a177aa61cdefe176fd13448
pathos <neopathos@me.com>
2020-06-12 00:59:18 +0900

    무중단 배포 스크립트 오류 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,27 +5,27 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=springboot2-webservice
+PROJECT_NAME=freelec-springboot2-webservice
 
-echo "> Copy build files"
+echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> deploying new application"
+echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-echo "> chmod to $JAR_NAME "
+echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
-echo "> running $JAR_NAME"
+echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
-echo "> running $JAR_NAME with  profile=$IDLE_PROFILE"
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
     -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
     -Dspring.profiles.active=$IDLE_PROFILE \
--- scripts/switch.sh
+++ scripts/switch.sh
@@ -7,10 +7,10 @@ source ${ABSDIR}/profile.sh
 function switch_proxy() {
     IDLE_PORT=$(find_idle_port)
 
-    echo "> target Port: $IDLE_PORT"
-    echo "> switching port"
+    echo "> 전환할 Port: $IDLE_PORT"
+    echo "> Port 전환"
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
-    echo "> Reload nginx"
+    echo "> 엔진엑스 Reload"
     sudo service nginx reload
 }
