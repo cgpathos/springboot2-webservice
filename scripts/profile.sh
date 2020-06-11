ca85ef8239fe51873b4e261d7062188b7ac11932
pathos <neopathos@me.com>
2020-06-12 00:52:28 +0900

    무중단 배포 스크립트 오류 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -2,24 +2,23 @@
 
 function find_idle_profile()
 {
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-  if [ ${RESPONSE_CODE} -ge 400 ]
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
+    then
+        CURRENT_PROFILE=real2
+    else
+        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+    fi
 
-  then
-    CURRENT_PROFILE=real2
-  else
-    CURRENT_PROFILE=$(curl -s http://localhost/profile)
-  fi
+    if [ ${CURRENT_PROFILE} == real1 ]
+    then
+      IDLE_PROFILE=real2
+    else
+      IDLE_PROFILE=real1
+    fi
 
-  if [ ${CURRENT_PROFILE} == real1 ]
-  then
-    IDLE_PROFILE=real2
-  else
-    IDLE_PROFILE=real1
-  fi
-
-  echo "${IDLE_PROFILE}"
+    echo "${IDLE_PROFILE}"
 }
 
 function find_idle_port() {
